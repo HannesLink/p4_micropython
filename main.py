554650640f0f2c0f39125c042d2ff7395f7a4efc
Johannes Link <hannes1977@googlemail.com>
2026-01-11 17:58:53 +0100

    Complete metrics

--- main.py
+++ main.py
@@ -3,43 +3,73 @@ import _thread
 from serial import get_json
 
 app = Microdot()
+json_data = {}
 
-html = '''<!DOCTYPE html>
-<html>
-    <head>
-        <title>Microdot Example Page</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <div>
-            <h1>Microdot Example Page</h1>
-            <p>Hello from Microdot!</p>
-            <p><a href="/shutdown">Click to shutdown the server</a></p>
-        </div>
-    </body>
-</html>
-'''
-
-def create_temperatures():
-    json_data = get_json()
+def create_temperatures(d):
     output = [
         "# TYPE p4_temperatures_celsius gauge",
         "# UNIT p4_temperatures_celsius celsius",
         "# HELP p4_temperatures_celsius collected via RS232 from P4 Pellets"
         ]
-    for key in json_data:
+    for key in d:
         sensor = json_data[key]['sensor']
         total = json_data[key]['total']
-        factor = float(json_data[key]['factor'])
         if key in ['2', '3', '11', '17', '18', '20', '21', '23', '24', '25', '28']:
             metric = "p4_temperatures_celsius{sensor='%s'} %.2f" % (sensor, total)
             output.append(metric)
-        #output.append(key)
+    return '\n'.join(output)
+
+def create_states(d):
+    output = [
+        "# TYPE p4_states stateset",
+        "# HELP p4_states collected via RS232 from P4 Pellets"
+        ]
+    for key in d:
+        sensor = json_data[key]['sensor']
+        value = json_data[key]['value']
+        if key in ['26', '27']:
+            metric = "p4_states{sensor='%s'} %.1d" % (sensor, value)
+            output.append(metric)
+    return '\n'.join(output)
+
+def create_values(d):
+    output = [
+        "# TYPE p4_values gauge",
+        "# HELP p4_values collected via RS232 from P4 Pellets"
+        ]
+    for key in d:
+        sensor = json_data[key]['sensor']
+        total = json_data[key]['total']
+        if key in ['4', '5', '6', '7', '8', '9', '12', '13', '14', '22', '30']:
+            metric = "p4_values{sensor='%s'} %.2f" % (sensor, total)
+            output.append(metric)
+    return '\n'.join(output)
+
+def create_info(d):
+    status = ""
+    fehler = ""
+    output = [
+        "# TYPE p4_info info"
+        ]
+    for key in d:
+        sensor = json_data[key]['sensor']
+        total = json_data[key]['total']
+        if key == '1':
+            status = sensor
+        if key == '99':
+            fehler = total
+    metric = "p4_info{status='%s',fehler='%s'} 1" % (status, fehler)
+    output.append(metric)
     return '\n'.join(output)
 
 def create_metrics():
+    global json_data
+    json_data = get_json()
     output = []
-    output.append(create_temperatures())
+    output.append(create_temperatures(json_data))
+    output.append(create_states(json_data))
+    output.append(create_values(json_data))
+    output.append(create_info(json_data))
     return '\n'.join(output)
 
 @app.route('/metrics')
